a2872a6cb2596a39ef090e7f246a04d075827c52
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-29 16:27:50 +0530

    Updated: EmployeeWage.java File With UC2

--- src/com/Day_8_Employee_Wage/EmployeeWage.java
+++ src/com/Day_8_Employee_Wage/EmployeeWage.java
@@ -5,15 +5,21 @@ public class EmployeeWage
 	public static void main(String[] args) 
 	{
 		int IS_PRESENT = 1;
+		int EMP_RATE_PER_HR = 20;
+		int empHrs = 0;
 		int empCheck = (int)(Math.random()*10)%2;
 		System.out.println(+empCheck);
 		if(empCheck == IS_PRESENT)
 		{
 			System.out.println("Employee is Present");
+			empHrs=8;
 		}
 		else
 		{
 			System.out.println("Employee is Absent");
+			empHrs=0;
 		}
+		int empWage = empHrs*EMP_RATE_PER_HR;
+		System.out.println("Employee Daily Wage is: "+empWage);
 	}
 }

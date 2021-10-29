a6e50802f81580a4298b6c2df700c7452d21d754
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-29 16:47:35 +0530

    Updated: EmployeeWage.java File With UC5

--- src/com/Day_8_Employee_Wage/EmployeeWage.java
+++ src/com/Day_8_Employee_Wage/EmployeeWage.java
@@ -5,8 +5,10 @@ public class EmployeeWage
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
+	public static final int TOTAL_WORKING_DAYS = 20;
 	public static void main(String[] args) 
-	{		
+	{
+		
 		int empHrs = 0;
 		int empCheck =(int) ((Math.random())*10)%3;
 		System.out.println(+empCheck);
@@ -25,7 +27,9 @@ public class EmployeeWage
 			empHrs = 0;
 			break;
 		}
-		int empWage = empHrs*EMP_RATE_PER_HR;
-		System.out.println("Employee Daily Wage is: "+empWage);		
+		int dailyWage = empHrs*EMP_RATE_PER_HR;
+		System.out.println("Employee Daily Wage is: "+dailyWage);
+		int totalWage = dailyWage*TOTAL_WORKING_DAYS;
+		System.out.println("Total Monthly Wage is: "+totalWage);
 	}
 }
